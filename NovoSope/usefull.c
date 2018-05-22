247bf04c78de42e8dfca43cd4fa0762b2b6cfb95
wortz <jp_pp_hi5@hotmail.com>
2018-05-22 03:32:40 +0100

    Update usefull.c

--- NovoSope/usefull.c
+++ NovoSope/usefull.c
@@ -1,10 +1,8 @@
 #include "usefull.h"
 
 //Function that writes the message to the filename.txt
-void writeMessage(char *filename, char *message){
+void writeMessage(FILE *file, char *message){
 
-   FILE *file;
-   file = fopen(filename, "a+");
    fprintf(file, "%s\n",message);
 }
 
@@ -15,4 +13,28 @@ void cleanMessages(){
    fopen("cbook.txt", "w");
    fopen("slog.txt", "w");
    fopen("clog.txt", "w");
+}
+
+char * errorToChar(int error){
+	char ret[3];
+	switch(error){
+		case -1:sprintf(ret,"MAX");
+			break;
+		case -2:sprintf(ret,"NST");
+			break;
+		case -3:sprintf(ret,"IID");
+			break;
+		case -4:sprintf(ret,"ERR");
+			break;
+		case -5:sprintf(ret,"NAV");
+			break;
+		case -6:sprintf(ret,"FUL");
+			break;
+		case -7:sprintf(ret,"OUT");
+			break;
+		default:sprintf(ret,"NOP");
+			break;
+	}
+	char * ret1=ret;
+	return ret1;
 }
