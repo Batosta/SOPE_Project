1c5c2fa01a0970e108500c1f34627f79aeab6d6e
João Nuno Ferreira <up201605330@g.uporto.pt>
2018-05-23 00:10:01 +0100

    Update server.h

--- NovoSope/server.h
+++ NovoSope/server.h
@@ -8,20 +8,21 @@ void readRequest();
 void closeRequestFIFO();
 
 //Functions for the Answer FIFO
-void openAnswerFIFO(int pid, struct Answer ans);
-void sendAnswer(int ANSWER_FD, struct Answer ans);
+void openAnswerFIFO(int pid, struct Answer * ans);
+void sendAnswer(int ANSWER_FD, struct Answer * ans);
 
 //Functions for the threads
 void createTicketOfficeThread(int ticketOfficesNumber);
 void * ticketOfficeThread(void *arg);
 void readBuffer(struct Request * req);
-void processRequest(struct Request * req);
+void processRequest(struct Request * req, int id);
 int testSomeCond(struct Request * req);
 void freeSeat(struct Seat *seats, int seatNum);
 void bookSeat(struct Seat *seats, int seatNum, int clientId);
 int isSeatFree(struct Seat *seats, int seatNum);
+void handleAnswer(struct Answer * ans);
 
 //Functions for writing on the files
-void handleAnswer();
+void openLog();
+void writeLog(struct Answer * ans);
 void writeBook();
-void writeLog();
